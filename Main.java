a92cf87395e5cff515a7bbea362f020930c8ff6d
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-04-24 01:17:13 +0500

    2nd commit

--- Main.java
+++ Main.java
@@ -1,6 +1,60 @@
+class Animal
+{
+    private String name;
+
+    Animal(String name)
+    {
+        this.name = name;
+    }
+
+    String getName()
+    {
+        return name;
+    }
+
+    void makeSound()
+    {
+        System.out.println("Generic Sound");
+    }
+}
+
+class Dog extends Animal
+{
+    private String breed;
+
+    Dog(String name ,String breed)
+    {
+        super(name);
+        this.breed = breed;
+    }
+
+    @Override 
+    
+    void makeSound()
+    {
+        System.out.println("Bark");
+    }
+
+    String getBreed()
+    {
+        return breed;
+    }
+
+    void displayInfo()
+    {
+        System.out.println(getName());
+        System.out.println(getBreed());
+    }
+}
+
+
 public class Main {
     
     public static void main(String[] args) {
         
+        Dog dog = new Dog("Max", "Labrador");
+        dog.makeSound();
+
+        dog.displayInfo();
     }
 }
